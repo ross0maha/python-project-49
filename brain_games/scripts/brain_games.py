ca9970fdacedb31cf600ec6938a265f2c057741f
Dmitry Yuroff <yuroff.dmitry@gmail.com>
2024-01-06 16:53:43 +0300

    Brain Games app init

---
+++ brain_games/scripts/brain_games.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+def do_greetings() -> None:
+    """
+    Welcome function.
+    Just typing greeting.
+    """
+    print("Welcome to the Brain Games!")
+
+
+def main() -> None:
+    """Main entry function."""
+    do_greetings()
+
+
+if __name__ == "__main__":
+    main()
