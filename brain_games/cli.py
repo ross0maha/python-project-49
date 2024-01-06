a3f113df5972b0fd02d88733e95f3a3a89db58ab
Dmitry Yuroff <yuroff.dmitry@gmail.com>
2024-01-07 01:35:22 +0300

    For Brain-Even welcome_user function mod

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,9 +1,10 @@
 import prompt
 
 
-def welcome_user() -> None:
+def welcome_user() -> str:
     """
     Welcome user funcion
     """
     name = prompt.string('May I have your name? ')
     print(f'Hello, {name}!')
+    return name
