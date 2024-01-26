ab388b662c7b8556e51cbc4d3e7d348be1d7689f
Dmitry Yuroff <yuroff.dmitry@gmail.com>
2024-01-26 21:06:18 +0300

    update flake8 rules and fix lint error

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -12,6 +12,7 @@ def welcome_user() -> str:
 def bad_answer(true_answer: str, user_answer: str, user_name: str) -> None:
     """Function for bad answer end exit game"""
 
-    print(f"'{user_answer}' is wrong answer ;(. Correct answer was '{true_answer}'.")
+    print(f"'{user_answer}' is wrong answer ;(. "
+           "Correct answer was '{true_answer}'.")
     print(f"Let's try again, {user_name}")
     exit()
