e53ba87c2fc3d1c826fab5b03453d96d12e6500e
Dmitry Yuroff <yuroff.dmitry@gmail.com>
2024-02-02 00:41:48 +0300

    prepare for refactoring the project structure. add engine and CONST file

---
+++ brain_games/const.py
@@ -0,0 +1,8 @@
+EVEN_PROMT = 'Answer "yes" if the number is even, otherwise answer "no".'
+CALC_PROMT = 'What is the result of the expression?'
+GCD_PROMT = 'Find the greatest common divisor of given numbers.'
+PROGRESSION_PROMT = 'What number is missing in the progression?'
+PRIME_PROMT = 'Answer "yes" if given number is prime. ' \
+              'Otherwise answer "no".'
+AMOUNT_OF_ROUNDS = 3
+OPERATORS = ['+', '-', '*']
---
+++ brain_games/engine.py
@@ -0,0 +1,22 @@
+import prompt
+from brain_games.const import AMOUNT_OF_ROUNDS
+
+
+def run_game(get_question_and_answer, game_promt):
+    user_name = prompt.string("Welcome to the Brain Games!\n"
+                              "May i have you name? ")
+    print(f"Hello, {user_name}!\n"
+          f"{game_promt}")
+
+    for _ in range(AMOUNT_OF_ROUNDS):
+        question, correct_answer = get_question_and_answer()
+        user_answer = prompt.string(f"Question: {question}\n"
+                                    "Your answer: ")
+        if user_answer == correct_answer:
+            print("Correct!")
+        else:
+            print(f"'{user_answer}' is wrong answer ;(. "
+                  f"Correct answer is '{correct_answer}'.\n"
+                  f"Let's try again, {user_name}!")
+            return
+    print(f"Congritulation, {user_name}")
