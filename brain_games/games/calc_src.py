7beae5df39489c30b747a0bf0b876295b3d43749
Dmitry Yuroff <yuroff.dmitry@gmail.com>
2024-02-02 23:07:46 +0300

    linter error fix

--- brain_games/games/calc_src.py
+++ brain_games/games/calc_src.py
@@ -7,15 +7,15 @@ def get_eval_and_answer():
     num1, num2 = random.randint(1, 50), random.randint(1, 50)
     operator = random.choice(OPERATORS)
     question = f'{num1} {operator} {num2}'
-    
+
     match operator:
         case '+':
             correct_answer = num1 + num2
         case '-':
             correct_answer = num1 - num2
         case '*':
-            correct_answer = num1 * num2    
-    
+            correct_answer = num1 * num2
+
     return (question, str(correct_answer))
 
 
