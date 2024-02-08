340c1f337420df4119b3ab34e0c01e39a30fe13b
Dmitry Yuroff <yuroff.dmitry@gmail.com>
2024-02-09 00:47:18 +0300

    decomposition brain-progression game

--- brain_games/games/progression_src.py
+++ brain_games/games/progression_src.py
@@ -4,44 +4,38 @@ from brain_games.const import STEP_FROM, STEP_TO
 from brain_games.const import PROG_START, PROG_END
 
 
-def do_rand() -> tuple:
-    """
-    Function returned tuple:
-        result - random math progression
-        miss_position - position of miss numder in progression
-        miss_num - random numder for question
-    """
+def generate_question_string(string_lenght, string_step, string_start):
 
-    length = random.randint(LENGTH_START, LENGTH_END)
-    step = random.randint(STEP_FROM, STEP_TO)
-    start = random.randint(PROG_START, PROG_END)
-    result = list(range(start, start + step * length, step))
-    miss_position = random.randint(0, length - 1)
-    miss_num = result[miss_position]
+    question_string = list(range(string_start, string_start + string_step
+                                 * string_lenght, string_step))
 
-    return result, miss_position, miss_num
+    return question_string
 
 
-def question_list_to_string(list_seq, miss_position) -> str:
-    """
-    Function for prepare random list to question string
-    """
+def question_list_to_string(list_seq, position):
 
-    result = ""
-    for elem in enumerate(list_seq):
-        if elem[0] == miss_position:
-            result += ".. "
-        else:
-            result += str(elem[1]) + " "
+    result = list(map(lambda x: str(x), list_seq))
+    result[position] = ".."
+    result = " ".join(result)
+
     return result
 
 
 def get_question_and_answer():
-    question_seq, miss_position, true_answer = do_rand()
-    question_str = question_list_to_string(question_seq, miss_position)
+    question_string_length = random.randint(LENGTH_START, LENGTH_END)
+    question_string_step = random.randint(STEP_FROM, STEP_TO)
+    question_string_start = random.randint(PROG_START, PROG_END)
+    miss_position = random.randint(0, question_string_length - 1)
+
+    question_seq = generate_question_string(question_string_length,
+                                            question_string_step,
+                                            question_string_start)
+
+    question_str = question_list_to_string(question_seq.copy(), miss_position)
+    true_answer = question_seq[miss_position]
+
     return question_str, str(true_answer)
 
 
 def run_progression_game():
-
     return get_question_and_answer
